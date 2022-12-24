3b3ddb695604d39276593f99f7f909e7304293d2
lim-jiwoo <il04131@naver.com>
2022-12-24 12:51:45 +0900

    FEAT : AddGroup #11 allow Nav to fill the parent component
    Button 컴포넌트 안에 위치했을 때
    부모 컴포넌트를 다 채우지 않아
    버튼 일부는 클릭되지 않는 문제가 있어
    fill 속성으로 부모를 채울 수 있게 했음

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,16 +7,30 @@ interface LinkProps {
     weight?: 'bold';
     color?: 'black' | 'white';
     underLine?: true | false;
+    fill?: boolean;
     children: React.ReactNode;
     url: string;
 }
 
-const Nav = ({ size, weight, color, underLine, children, url }: LinkProps) => {
+const Nav = ({
+    size,
+    weight,
+    color,
+    underLine,
+    fill,
+    children,
+    url,
+}: LinkProps) => {
     const Component = styled(Link)`
         font-size: ${({ theme }) => theme.font.size[size ?? 'small']}px;
         font-weight: ${({ theme }) => theme.font.weight[weight ?? 'bold']};
         color: ${color};
         text-decoration: ${underLine ? 'underline' : 'none'};
+        display: block;
+        height: ${fill && '100%'};
+        display: flex;
+        justify-content: center;
+        align-items: center;
     `;
     return <Component to={url}>{children}</Component>;
 };
