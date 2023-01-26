badc27a71eda545163f0da83f338debd21450165
lim-jiwoo <il04131@naver.com>
2023-01-26 11:35:30 +0900

    FEAT : Navbar #6 implement axios interceptor
    원래 구현한 대로는 access token이 바뀌어도 초기값으로만
    보내져서 interceptor에서 매번 최신값으로 가져오게 구현함

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
-import { useToken } from '../hooks';
+import storage from '../lib/storage';
 
 const API = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
-    headers: {
-        Authorization: useToken(),
-    },
+});
+
+API.interceptors.request.use(function (config) {
+    const token = storage.getEntry('token') as string | null;
+    config.headers = config.headers ?? {};
+    config.headers['Authorization'] = token ?? '';
+    return config;
 });
 
 export default API;
