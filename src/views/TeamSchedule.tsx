89b3364cf70acbf8e9ca6da4b2ca62544f402984
lim-jiwoo <il04131@naver.com>
2023-02-01 17:43:00 +0900

    FEAT : schedule #20 remove Daypilot calendar library

--- src/views/TeamSchedule.tsx
+++ src/views/TeamSchedule.tsx
@@ -1,32 +1,15 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from '@emotion/styled/macro';
-import { DayPilot, DayPilotCalendar } from '@daypilot/daypilot-lite-react';
 import { Button, Nav, Members } from '../components';
 import { useTeamInfo } from '../hooks';
-import { generateColor } from '../hooks/ColorMethod';
 import {
     Container,
     Header,
     Field,
     AlignRight,
 } from '../styles/globalStyle/PageLayout';
-import { MainSchedule, MemberBox } from '../styles/schedule/schedule';
-
-const data = [
-    {
-        id: 1,
-        start: DayPilot.Date.today().addHours(12),
-        end: DayPilot.Date.today().addHours(13),
-        text: 'Event 1',
-    },
-    {
-        id: 2,
-        start: DayPilot.Date.today().addHours(18),
-        end: DayPilot.Date.today().addHours(20),
-        text: 'Event 2',
-    },
-];
+import { MainSchedule } from '../styles/schedule/schedule';
 
 export const IconBox = styled.div`
     display: flex;
@@ -36,68 +19,10 @@ export const IconBox = styled.div`
     cursor: pointer;
 `;
 
-const dummy = ['강정구', '진지연', '송민진', '임지우', '권영재', '김철수'];
-
 const TeamSchedule: React.FC = () => {
     const params = useParams();
     const { teamInfo, error } = useTeamInfo(Number(params.teamId));
-    const calendarRef: any = useRef();
-    const todayDate = DayPilot.Date.today();
-    const newmodal = DayPilot.Modal;
     const [name, setName] = useState('');
-    // const init = (args) => (args.control.events.list = data);
-    // useEffect(() => {
-    //     calendarRef.control.events.list = data;
-    // }, []);
-    const state = {
-        viewType: 'Week',
-        durationBarVisible: false,
-        timeRangeSelectedHandling: 'Enabled',
-        dayBeginsHour: 9,
-        businessEndsHour: 24,
-        heightSpec: 'BusinessHoursNoScroll',
-        onBeforeHeaderRender: (args: any) => {
-            args.header.html = args.heaer.start.toString('yyyy/M/d');
-        },
-        onTimeRangeSelected: async (args: any) => {
-            console.log('args : ', args);
-            const dp = args.control;
-            console.log('dp : ', dp);
-            // dp.update({ startDate, data });
-            const modal = await newmodal.prompt(
-                '생성할 스케줄 이름 : ',
-                'Schedule 1',
-                { theme: 'modal_rounded' },
-            );
-            dp.clearSelection();
-            if (!modal.result) {
-                return;
-            }
-            dp.events.add({
-                start: args.start,
-                end: args.end,
-                id: DayPilot.guid(),
-                text: modal.result,
-            });
-            console.log(args.start);
-            console.log(args.end);
-        },
-        eventDeleteHandling: 'Update',
-        onEventClick: async (args: any) => {
-            const dp = args.control;
-            const modal = await DayPilot.Modal.prompt(
-                '변경할 스케줄 이름 : ',
-                args.e.text(),
-                { theme: 'modal_rounded' },
-            );
-            if (!modal.result) {
-                return;
-            }
-            const e = args.e;
-            e.data.text = modal.result;
-            dp.events.update(e);
-        },
-    };
 
     return (
         <Container>
@@ -120,14 +45,6 @@ const TeamSchedule: React.FC = () => {
                 </AlignRight>
             </Header>
             <MainSchedule name={name}>
-                <DayPilotCalendar
-                    days={7}
-                    startDate={todayDate}
-                    ref={calendarRef}
-                    cellHeight={30}
-                    cellDuration={30}
-                    {...state}
-                />
             </MainSchedule>
             <Field>
                 <h3>그룹원</h3>
