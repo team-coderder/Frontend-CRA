64b3b45575cf8b8836eed7e5542562bc1dde0ea8
lim-jiwoo <il04131@naver.com>
2023-02-04 19:39:05 +0900

    FIX : group-info #18 fix because axiosError instance has message not localizedMessage

--- src/utils/common/handleError.ts
+++ src/utils/common/handleError.ts
@@ -1,8 +1,8 @@
 import { AxiosError } from 'axios';
 
 export function handleError(error: unknown) {
-    if (error instanceof AxiosError && error.response?.data?.localizedMessage) {
-        alert(error.response?.data?.localizedMessage);
+    if (error instanceof AxiosError && error.response?.data?.message) {
+        alert(error.response?.data?.message);
     } else {
         console.log(error);
     }
