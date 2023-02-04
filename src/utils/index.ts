c3e1da84031a6e18786bef8d0e4a49cebb93b257
lim-jiwoo <il04131@naver.com>
2023-02-04 17:28:24 +0900

    FEAT : schedule #20 implement user/team schedule SWR hooks

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
 // common
 export { onClickOutside } from './common/onClickOutside';
 export { generateColor } from './common/generateColor';
+export { handleError } from './common/handleError';
+// schedule
+export {
+    generateDateFromString,
+    generateStringFromDate,
+} from './schedule/convertDate';
 // team
 export { isNameValid } from './team/isNameValid';
